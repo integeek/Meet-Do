2908d45e6b1a226b7f8a9d3a94cada56c633bc37
integeek <ambre.giraudlanza@outlook.fr>
2025-03-12 14:18:32 +0100

    pop up mot de passe oublié

--- Script/AfficherPass.js
+++ Script/AfficherPass.js
@@ -1,10 +1,6 @@
-// Sélectionne tous les boutons d'affichage de mot de passe
 document.querySelectorAll(".togglePassword").forEach((toggle) => {
     toggle.addEventListener("click", function() {
-        // Trouve l'input associé
         let passwordField = this.previousElementSibling;
-
-        // Alterne entre "password" et "text"
         if (passwordField.type === "password") {
             passwordField.type = "text";
             this.src = "../assets/img/ferme.png"; 
