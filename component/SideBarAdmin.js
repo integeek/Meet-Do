5a01d13c258ad5d808ed3805dcf23ea8c21dbacc
integeek <ambre.giraudlanza@outlook.fr>
2025-03-20 14:05:32 +0100

    component sidebar admin

---
+++ component/SideBarAdmin.js
@@ -0,0 +1,15 @@
+function SideBarAdmin() {
+    return `
+        <div class="main-sidelinks">
+            <ul class="side-links">
+                <li><a href="#">Tableau de bord</a></li>
+                <li><a href="#">Gestion des clients</a></li>
+                <li><a href="#">Messagerie</a></li>
+                <li><a href="#">Signalement utilisateur</a></li>
+                <li><a href="#">Signalement annonce</a></li>
+                <li><a href="#">Accepter meeters</a></li>
+                <li><a href="#">Modifier les tables</a></li>
+            </ul>
+        </div>
+`;
+};
