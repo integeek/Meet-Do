aab35167effcbb8e531afef5274fc4d73c7ba7cc
integeek <ambre.giraudlanza@outlook.fr>
2025-03-30 18:24:18 +0200

    search bar admin

---
+++ component/SearchBarAdmin/SearchBarAdmin.js
@@ -0,0 +1,13 @@
+function SearchBarAdmin(){
+    return `
+    <div class="search-number">
+                    <div class="search-container">
+                        <input type="text" class="search-input" placeholder="Rechercher">
+                        <img class="search-icon" src="../assets/img/icons/search-icon.svg" alt="">
+                    </div>
+                    <div class="client-count">
+                        Nombre de clients : <span id="clientNumber">10</span>
+                    </div>
+                </div>
+    `
+}
