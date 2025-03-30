a3d768cc8c5264f90dde120621e6aa79405d9b45
integeek <ambre.giraudlanza@outlook.fr>
2025-03-30 18:24:01 +0200

    mise à jour composant

--- component/Pagination/Pagination.js
+++ component/Pagination/Pagination.js
@@ -1,16 +1,16 @@
 function Pagination(){
     return `
     <div class="pagination">
-        <button class="btn1" onclick="backBtn()"> <img src="../assets/img/icons/arrow-icon.svg">prev</button>
-        <ul>
-            <li class="link active" value="1" onclick="activeLink()">1</li>
-            <li class="link" value="2" onclick="activeLink()">2</li>
-            <li class="link" value="3" onclick="activeLink()">3</li>
-            <li class="link" value="4" onclick="activeLink()">4</li>
-            <li class="link" value="5" onclick="activeLink()">5</li>
-            <li class="link" value="6" onclick="activeLink()">6</li>
-        </ul>
-        <button class="btn2" onclick="nextBtn()"> next<img src="../assets/img/icons/arrow-icon.svg"></button>
-    </div>
+            <button class="btn1" onclick="backBtn()"> <img src="../assets/img/icons/arrow-icon.svg">prev</button>
+            <ul>
+                <li class="link active" value="1" onclick="activeLink()"><a class="page-link" href="#">1</a></li>
+                <li class="link" value="2" onclick="activeLink()"><a class="page-link" href="">2</a></li>
+                <li class="link" value="3" onclick="activeLink()"><a class="page-link" href="">3</a></li>
+                <li class="link" value="4" onclick="activeLink()"><a class="page-link" href="">4</a></li>
+                <li class="link" value="5" onclick="activeLink()"><a class="page-link" href="">5</a></li>
+                <li class="link" value="6" onclick="activeLink()"><a class="page-link" href="">6</a></li>
+            </ul>
+            <button class="btn2" onclick="nextBtn()"> next<img src="../assets/img/icons/arrow-icon.svg"></button>
+        </div>
     `
 }
