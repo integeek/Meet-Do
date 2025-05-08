c869c3e1894b8eebf1f422aa9f43fd09a24bb07c
Alexis <desj75a@gmail.com>
2025-05-08 04:20:37 +0200

    TableauBord

---
+++ controller/TableauBord/GetData.php
@@ -0,0 +1,54 @@
+<?php
+require_once("../../model/Bdd.php");
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $sql1 = "SELECT COUNT(*) AS 'number' FROM `Client`;";
+    $query = $db->prepare($sql1);
+    $query->execute();
+    $nombreClient = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    $sql2 = "SELECT COUNT(*) AS 'number' FROM `Activite`;";
+    $query = $db->prepare($sql2);
+    $query->execute();
+    $nombreActivite = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    $sql3 = "SELECT enumActivites AS 'activity', COUNT(*) AS 'number'  FROM Activite GROUP BY enumActivites;";
+    $query = $db->prepare($sql3);
+    $query->execute();
+    $nombreActiviteTheme = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    $sql4 = "
+                SELECT    
+            SUM(CASE MONTH(dateCreation) WHEN 1 THEN 1 ELSE 0 END) AS 'janvier',
+            SUM(CASE MONTH(dateCreation) WHEN 1 THEN 1 ELSE 0 END) AS 'fevrier',
+            SUM(CASE MONTH(dateCreation) WHEN 1 THEN 1 ELSE 0 END) AS 'mars',
+            SUM(CASE MONTH(dateCreation) WHEN 1 THEN 1 ELSE 0 END) AS 'avril',
+            SUM(CASE MONTH(dateCreation) WHEN 5 THEN 1 ELSE 0 END) AS 'mai',
+            SUM(CASE MONTH(dateCreation) WHEN 1 THEN 1 ELSE 0 END) AS 'juin',
+            SUM(CASE MONTH(dateCreation) WHEN 1 THEN 1 ELSE 0 END) AS 'juillet',
+            SUM(CASE MONTH(dateCreation) WHEN 1 THEN 1 ELSE 0 END) AS 'aout',
+            SUM(CASE MONTH(dateCreation) WHEN 1 THEN 1 ELSE 0 END) AS 'septembre',
+            SUM(CASE MONTH(dateCreation) WHEN 1 THEN 1 ELSE 0 END) AS 'octobre',
+            SUM(CASE MONTH(dateCreation) WHEN 1 THEN 1 ELSE 0 END) AS 'novembre',
+            SUM(CASE MONTH(dateCreation) WHEN 1 THEN 1 ELSE 0 END) AS 'decembre'
+            FROM Activite;";
+    $query = $db->prepare($sql4);
+    $query->execute();
+    $nombreActiviteMois = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    $jsonData = [
+        "nombreClient" => $nombreClient,
+        "nombreActivite" => $nombreActivite,
+        "nombreActiviteTheme" => $nombreActiviteTheme,
+        "nombreActiviteParMois" => $nombreActiviteMois
+    ];
+
+
+    if (empty($jsonData)) {
+        echo json_encode(["message" => "La table est vide."]);
+    } else {
+        header('Content-Type: application/json');
+        echo json_encode($jsonData, JSON_UNESCAPED_UNICODE);
+    }
+} else {
+    exit();
+}
