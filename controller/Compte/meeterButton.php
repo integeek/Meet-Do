e29a1f1a58daa09fe4660d7ed0020b9fa4041426
zeyluish <teogdn.pro@gmail.com>
2025-05-16 14:38:35 +0200

    permissions bouton meeter

---
+++ controller/Compte/meeterButton.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user']['email'])) {
+    header("Location: ../../view/page/Connexion.php");
+    exit;
+}
+
+require_once("../../model/Bdd.php");
+
+$idClient = $_SESSION['user']['id'];
+$sql = "SELECT role FROM Client WHERE idClient = :idClient";
+$stmt = $db->prepare($sql);
+$stmt->execute([':idClient' => $idClient]);
+
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
+if ($result) {
+    $role = $result['role'];
+} else {
+    echo "Erreur : impossible de récupérer le rôle de l'utilisateur.";
+    exit;
+}
+
+$hideDiv = '';
+
+if($role === 'Meeter' || $role === 'Administrateur') {
+    $hideDiv = '<style> .become-meeter {display: none;}</style>';
+}
+
+?>
