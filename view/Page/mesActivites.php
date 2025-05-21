1b0a167fdf715d36da1d43a63be7739e3fefbf57
zeyluish <teogdn.pro@gmail.com>
2025-05-21 20:43:38 +0200

    fichier css pour mes activités

--- view/Page/mesActivites.php
+++ view/Page/mesActivites.php
@@ -15,7 +15,7 @@ if (session_status() === PHP_SESSION_NONE) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../../view/Style/mesReservations.css">
+  <link rel="stylesheet" href="../../view/Style/mesActivites.css">
   <link rel="stylesheet" href="../../view/component/Navbar/Navbar.css" />
   <link rel="stylesheet" href="../../view/component/Footer/Footer.css" />
   <link rel="stylesheet" href="../../view/component/BoutonBleu.css" />
