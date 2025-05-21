4fa1b2059dc16a080b40fdc513cb324aa435fe8e
integeek <ambre.giraudlanza@outlook.fr>
2025-05-21 16:52:22 +0200

    mise en place file d'attente

--- Model/Reservation.php
+++ Model/Reservation.php
@@ -2,14 +2,26 @@
 require_once("Bdd.php");
 
 Class Reservation {
-    public static function makeReservation($nbPlace, $idClient, $idEvenement) {
+
+    public static function getPlacement($idEvenement) {
         $db = Bdd::getInstance();
+        $sql = "SELECT COALESCE(MAX(placement), 0) as lastPlacement 
+                FROM Reservation 
+                WHERE idEvenement = :idEvenement";
+        $query = $db->prepare($sql);
+        $query->execute([':idEvenement' => $idEvenement]);
+        return (int) $query->fetchColumn() + 1;
+    }
+    public static function makeReservation($nbPlace, $idClient, $idEvenement, $listeAttente = 0) {
+        $db = Bdd::getInstance();
+        $placement = self::getPlacement($idEvenement); // Appel de la fonction
         $sql = "INSERT INTO Reservation (dateReservation, nbPlace, listeAttente, placement, idClient, idEvenement) 
-        VALUES (NOW(), :nbPlace, :listeAttente, 0, :idClient, :idEvenement)";
+        VALUES (NOW(), :nbPlace, :listeAttente, :placement, :idClient, :idEvenement)";
         $query = $db->prepare($sql);
         $query->execute([
             ':nbPlace' => $nbPlace,
-            ':listeAttente' => 0,
+            ':listeAttente' => $listeAttente,
+            'placement' => $placement,
             ':idClient' => $idClient,
             ':idEvenement' => $idEvenement
         ]);
