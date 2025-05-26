27408b543a27d031878b809cb92a887979556b84
Alexis <desj75a@gmail.com>
2025-05-26 23:56:02 +0200

    ListeEvenement

---
+++ Model/ListeEvenementModel.php
@@ -0,0 +1,22 @@
+<?php
+// filepath: c:\xampp\htdocs\Meet-Do\Model\ListeEvenementModel.php
+require_once(__DIR__ . "/Bdd.php");
+
+class ListeEvenementModel {
+    private $db;
+    public function __construct($db) {
+        $this->db = $db;
+    }
+
+    public function getEvenements($idActivite) {
+        $sql = "SELECT Evenement.idEvenement, Evenement.dateEvenement, Activite.titre, Evenement.idEvenement
+                FROM Evenement
+                INNER JOIN Activite ON Activite.idActivite = Evenement.idActivite
+                WHERE Evenement.idActivite = :idActivite
+                ORDER BY Evenement.dateEvenement ASC";
+        $query = $this->db->prepare($sql);
+        $query->execute(['idActivite' => $idActivite]);
+        return $query->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+?>
